f49aaf7734698cc0b2f8c247d036b930cda6ab1a
Andrius Ivanickas <andrius.ivanickas9@gmail.com>
2023-10-11 20:54:06 +0300

    Add project files.

---
+++ savarankiskas4_4/Kates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savarankiskas4_4
+{
+    internal class Kates 
+    {
+
+        public string KatesVardas { get; set; }
+        public string KatesVeisle { get; set; }
+        public string KatesLytis { get; set; }
+        public int KatesAmzius { get; set; }
+        public int KatesSvoris { get; set; }
+        public Kates (string vardas, string veisle, string lytis, int amzius, int svoris)
+        {
+            KatesVardas = vardas;
+            KatesVeisle = veisle;
+            KatesLytis = lytis;
+            KatesAmzius = amzius;
+            KatesSvoris = svoris;
+        }
+        public override string ToString()
+        {
+            return string.Format($" {KatesVardas,7} {KatesVeisle,14} {KatesLytis,10} {KatesAmzius,4}  {KatesSvoris,6}");
+        }
+        
+    }
+}
---
+++ savarankiskas4_4/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace savarankiskas4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> Vardai = new List<string>() { "Tomas", "Benas", "Leja" };
+            Kates kate1 = new Kates("Tomas", "Bengalijos", "vyras", 5, 10);
+            Kates kate2 = new Kates("Benas", "Bombejaus", "vyras", 10, 15);
+            Kates kate3 = new Kates("Leja", "Brazilijos", "moteris", 7, 10);
+            List<Kates> katinai = new List<Kates>();
+
+            katinai.Add(kate1);
+            katinai.Add(kate2);
+            katinai.Add(kate3);
+            SpausdintiKatinus();
+            PrintList(katinai);
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+            Console.WriteLine("2 UZD");
+            Console.Write("Kuri irasa pasalinti: ");
+            string salinimas = Console.ReadLine();
+            Console.WriteLine();
+            Trinimas(katinai, salinimas);
+            PrintList(katinai);
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+            Console.WriteLine("3 UZD");
+            Console.Write("Ieskoma: ");
+            string paieska = Console.ReadLine();
+            Console.WriteLine($"Tokiu yra: {Paieska(katinai, paieska)}");
+
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+            Console.WriteLine("4 UZD");
+            Console.WriteLine($"vidutinis svoris: {VidutinisSvoris(katinai)}");
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+            Console.WriteLine("5 UZD");
+            katinai.Sort((x, y) => y.KatesSvoris.CompareTo(x.KatesSvoris));
+            PrintList(katinai);
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+
+            Console.ReadKey();
+
+
+
+
+        }
+        static void PrintList(List<Kates> katinai)
+        {
+            Console.WriteLine("| Vardas |     Veisle     | Lytis | Metai | Svoris |");
+            for (int i = 0; i < katinai.Count; i++)
+            {
+                Console.WriteLine(katinai[i]);
+            }
+        }
+
+        static void Trinimas(List<Kates> katinai, string salinimas)
+        {
+
+            int num = 0;
+            for (int i = 0; i < katinai.Count; i++)
+            {
+                
+                if (katinai[i].KatesVardas == salinimas)
+                {
+                    katinai.RemoveAt(i);
+
+                    num++;
+                   
+                }
+                
+                
+                
+
+
+            }
+            if (num == 0)
+            {
+                Console.WriteLine("Tokio iraso nera");
+            }
+        }
+        static double VidutinisSvoris(List<Kates> katinai)
+        {
+
+            double svoris = 0;
+            int kiekis = 0;
+            for (int i = 0; i < katinai.Count; i++)
+            {
+                svoris = svoris + katinai[i].KatesSvoris;
+                kiekis++;
+            }
+            return Math.Round( (svoris / kiekis),2);
+        }
+        static int Paieska(List<Kates> katinai, string paieska)
+        {
+            int kiekis = katinai.Count(elem => elem.KatesVardas.Equals(paieska));
+            return kiekis;
+        }
+        static public void SpausdintiKatinus()
+        {
+            
+        }
+    }
+}
+
+
